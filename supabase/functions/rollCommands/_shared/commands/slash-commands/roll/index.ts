6785ccd87d887164765e8e4f97376a2af4dbf945
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-06-16 22:14:11 -0400

    Validate dice notation

--- supabase/functions/rollCommands/_shared/commands/slash-commands/roll/index.ts
+++ supabase/functions/rollCommands/_shared/commands/slash-commands/roll/index.ts
@@ -2,7 +2,7 @@ import { EmbedBuilder } from "npm:@discordjs/builders"
 import {
   APIApplicationCommandInteraction,
 } from "https://deno.land/x/discord_api_types@0.37.71/v10.ts"
-import { roll } from "npm:randsum"
+import { roll, validateDiceNotation } from "npm:randsum"
 import { embedFooterDetails } from "../../../constants.ts"
 
 const buildEmbed = (
@@ -12,20 +12,22 @@ const buildEmbed = (
     (interaction.data as unknown as { options: [{ value: string }] }).options
       ?.[0]?.value
 
-  try {
-    const result = roll(notationArg)
-    return new EmbedBuilder()
-      .setTitle("Success")
-      .setDescription(`You rolled ${result.total}`)
-      .setFooter(embedFooterDetails)
-      .toJSON()
-  } catch {
+  const isValid = validateDiceNotation(notationArg)
+
+  if (!isValid) {
     return new EmbedBuilder()
       .setTitle("Error")
-      .setDescription("There was an error with your roll.")
+      .setDescription("Invalid dice notation.")
       .setFooter(embedFooterDetails)
       .toJSON()
   }
+
+  const result = roll(notationArg)
+  return new EmbedBuilder()
+    .setTitle("Success")
+    .setDescription(`You rolled ${result.total}`)
+    .setFooter(embedFooterDetails)
+    .toJSON()
 }
 
 export function handleRoll(
--- supabase/functions/rollCommands/_shared/constants.ts
+++ supabase/functions/rollCommands/_shared/constants.ts
@@ -1,4 +1,4 @@
 export const embedFooterDetails = {
   iconURL: "https://avatars.githubusercontent.com/u/23259900?s=96&v=4",
-  text: "Rolled with [RANDSUM](randsum.io)",
+  text: "Rolled with RANDSUM.io",
 }
