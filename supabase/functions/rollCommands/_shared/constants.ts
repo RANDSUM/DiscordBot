fd6dcfc574760264ee2848ba9662bfc7bd6d1d29
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-06-16 22:08:27 -0400

    Update icon

--- supabase/functions/rollCommands/_shared/constants.ts
+++ supabase/functions/rollCommands/_shared/constants.ts
@@ -1,4 +1,4 @@
 export const embedFooterDetails = {
-  iconURL: "https://randsum.io/favicon.ico",
+  iconURL: "https://avatars.githubusercontent.com/u/23259900?s=96&v=4",
   text: "Rolled with RANDSUM - randsum.io",
 }
