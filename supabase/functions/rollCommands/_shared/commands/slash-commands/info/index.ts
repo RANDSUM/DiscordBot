1fce7fa7170b3710ff2617e5865bb6ad7e219e00
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-06-19 08:20:24 -0400

    Tweak Info

--- supabase/functions/rollCommands/_shared/commands/slash-commands/info/index.ts
+++ supabase/functions/rollCommands/_shared/commands/slash-commands/info/index.ts
@@ -7,21 +7,34 @@ const buildEmbed = () => {
   return new EmbedBuilder()
     .setTitle("Randsum")
     .setDescription(
-      "RANDSUM.io is the internet's premier randum number service. Generate your own rolls with our [Dice Notation](https://github.com/RANDSUM/randsum-ts/blob/main/RANDSUM_DICE_NOTATION.md) or use some of our built-in game commands. You'll get the rolls you want - not always the results you need.",
+      "**RANDSUM.io** is the internet's premier randum number service. Generate your own rolls with our [Dice Notation](https://github.com/RANDSUM/randsum-ts/blob/main/RANDSUM_DICE_NOTATION.md) or use some of our built-in game commands.",
     )
-    .setFields({
-      name: "/roll <notation>",
-      value:
-        "Pass in dice notation - i.e., 2d6 + 3 - to generate a roll result.",
-    }, {
-      name: "/blades <dice>",
-      value:
-        "Make a roll in the Forged in the Dark system. Pass in the # of d6 to roll. If you are rolling 0, pass in 0, and we'll do the math for you.",
-    }, {
-      name: "/root <plus>",
-      value:
-        "Make a roll in the Root RPG system. Roll 2d6 and add the 'plus' value.",
-    })
+    .setFields(
+      {
+        name: "",
+        value:
+          "You'll get the rolls you want - not always the results you need.",
+      },
+      {
+        name: "Commands",
+        value: "",
+      },
+      {
+        name: "/roll <notation>",
+        value:
+          "Pass in dice notation - i.e., 2d6 + 3 - to generate a roll result.",
+      },
+      {
+        name: "/blades <dice>",
+        value:
+          "Make a roll in the Forged in the Dark system. Pass in the # of d6 to roll. If you are rolling 0, pass in 0, and we'll do the math for you.",
+      },
+      {
+        name: "/root <plus>",
+        value:
+          "Make a roll in the Root RPG system. Roll 2d6 and add the 'plus' value.",
+      },
+    )
     .setFooter(embedFooterDetails)
     .toJSON()
 }
