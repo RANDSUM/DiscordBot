5c2af5644b29bbf154174a1b4f7b3f3b8c01fabb
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-06-19 18:20:18 -0400

    Inline?

--- supabase/functions/rollCommands/_shared/commands/slash-commands/info/index.ts
+++ supabase/functions/rollCommands/_shared/commands/slash-commands/info/index.ts
@@ -8,16 +8,19 @@ const fields = [
     name: "/roll <notation>",
     value:
       "Pass in dice notation or use some of our built-in game commands. - i.e., 2d6 + 3 - to generate a roll result.\n\n\n",
+    inline: true,
   },
   {
     name: "/blades <dice>",
     value:
       "Make a roll in the Forged in the Dark system. Pass in the # of d6 to roll as <dice>, and we'll show you the results. If rolling zero, pass in zero!\n\n\n",
+    inline: true,
   },
   {
     name: "/root <modifier>",
     value:
       "Make a roll in the Root RPG system. Roll 2d6 and add the <modifier> value.\n\n\n",
+    inline: true,
   },
 ]
 
@@ -26,7 +29,7 @@ const buildEmbed = () => {
     .setTitle("RANDSUM.io")
     .setDescription(
       [
-        "**[RANDSUM.io](randsum.io)** is the internet's premier randum number service. Generate your own rolls with our [Dice Notation](https://github.com/RANDSUM/randsum-ts/blob/main/RANDSUM_DICE_NOTATION.md) or use some of our built-in game commands.",
+        "[**RANDSUM.io**](https://www.randsum.io) is the internet's premier randum number service. Generate your own rolls with our [Dice Notation](https://github.com/RANDSUM/randsum-ts/blob/main/RANDSUM_DICE_NOTATION.md) or use some of our built-in game commands.",
         "You'll get the rolls you want - not always the results you need.",
         "Here are the available commands:",
       ].join("\n\n"),
