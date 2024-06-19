57579abc52fc62c2bf6996adde985fc8337fe348
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-06-19 18:17:09 -0400

    Better info

--- supabase/functions/rollCommands/_shared/commands/slash-commands/info/index.ts
+++ supabase/functions/rollCommands/_shared/commands/slash-commands/info/index.ts
@@ -3,43 +3,43 @@ import { embedFooterDetails } from "../../../constants.ts"
 import { InteractionResponseType } from "https://deno.land/x/discord_api_types@0.37.73/v10.ts"
 import { json } from "https://deno.land/x/sift@0.6.0/mod.ts"
 
-const buildEmbeds = () => {
-  return [
-    new EmbedBuilder()
-      .setTitle("Randsum")
-      .setDescription(
-        "**RANDSUM.io** is the internet's premier randum number service. Generate your own rolls with our [Dice Notation](https://github.com/RANDSUM/randsum-ts/blob/main/RANDSUM_DICE_NOTATION.md) or use some of our built-in game commands.\n\nYou'll get the rolls you want - not always the results you need.",
-      )
-      .toJSON(),
-    new EmbedBuilder()
-      .setTitle("/roll <notation>")
-      .setDescription(
-        "Pass in [dice notation](https://github.com/RANDSUM/randsum-ts/blob/main/RANDSUM_DICE_NOTATION.md) or use some of our built-in game commands. - i.e., 2d6 + 3 - to generate a roll result.",
-      )
-      .toJSON(),
-    new EmbedBuilder()
-      .setTitle("/blades <dice>")
-      .setDescription(
-        "Make a roll in the Forged in the Dark system. Pass in the # of d6 to roll, and we'll show you the results. If rolling zero, pass in zero!",
-      )
-      .toJSON(),
-    new EmbedBuilder()
-      .setTitle("/root <modifier>")
-      .setDescription(
-        "Make a roll in the Root RPG system. Roll 2d6 and add the <modifier> value.",
-      )
-      .toJSON(),
-    new EmbedBuilder()
-      .setFooter(embedFooterDetails)
-      .toJSON(),
-  ]
+const fields = [
+  {
+    name: "/roll <notation>",
+    value:
+      "Pass in dice notation or use some of our built-in game commands. - i.e., 2d6 + 3 - to generate a roll result.",
+  },
+  {
+    name: "/blades <dice>",
+    value:
+      "Make a roll in the Forged in the Dark system. Pass in the # of d6 to roll as <dice>, and we'll show you the results. If rolling zero, pass in zero!",
+  },
+  {
+    name: "/root <modifier>",
+    value:
+      "Make a roll in the Root RPG system. Roll 2d6 and add the <modifier> value.",
+  },
+]
+const buildEmbed = () => {
+  return new EmbedBuilder()
+    .setTitle("Randsum")
+    .setDescription(
+      [
+        "**RANDSUM.io** is the internet's premier randum number service. Generate your own rolls with our [Dice Notation](https://github.com/RANDSUM/randsum-ts/blob/main/RANDSUM_DICE_NOTATION.md) or use some of our built-in game commands.",
+        "You'll get the rolls you want - not always the results you need.",
+        "Here are the available commands:",
+      ].join("\n\n"),
+    )
+    .setFields(fields)
+    .setFooter(embedFooterDetails)
+    .toJSON()
 }
 
 export function handleInfo() {
   return json({
     type: InteractionResponseType.ChannelMessageWithSource,
     data: {
-      embeds: buildEmbeds(),
+      embeds: [buildEmbed()],
     },
   })
 }
