c46938f42f4e3a3d69b239c37495ca9c14f230c1
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-06-19 16:48:16 -0400

    No newlines

--- supabase/functions/rollCommands/_shared/commands/slash-commands/root/getSuccessString.ts
+++ supabase/functions/rollCommands/_shared/commands/slash-commands/root/getSuccessString.ts
@@ -5,17 +5,17 @@ export const getSuccessString = (type: RootRollType): string[] => {
     case RootRollType.StrongHit:
       return [
         "__**Strong Hit**__",
-        "*You'll get almost all you wanted,\nor some additional benefit.*",
+        "*You'll get almost all you wanted, or some additional benefit.*",
       ]
     case RootRollType.WeakHit:
       return [
         "__**Weak Hit**__",
-        "*You'll get almost all of what you wanted,\nbut usually with some cost or complication attached.*",
+        "*You'll get almost all of what you wanted, but usually with some cost or complication attached.*",
       ]
     case RootRollType.Miss:
       return [
         "__**Miss**__",
-        "*The GM gets to say what happens next,\nwith an eye towards complicating the situation dramatically.*",
+        "*The GM gets to say what happens next, with an eye towards complicating the situation dramatically.*",
       ]
   }
 }
